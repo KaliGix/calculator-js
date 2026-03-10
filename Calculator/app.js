71c2053d3fb0b4bce4d65404e2758aa7407faa3d
Kalibell Dalmasi <kalibelldalmasi@gmail.com>
2026-03-10 15:10:51 -0400

    Update functions

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,97 +1,98 @@
-
 const screen = document.querySelector(".screen");
 const buttonResult = document.querySelector(".result");
 const operatorButtons = document.querySelectorAll(".operator");
 const numberButtons = document.querySelectorAll(".addnumer");
 
-numberButtons.forEach(btn => {
-    btn.addEventListener("click",addNumber);
+numberButtons.forEach((btn) => {
+  btn.addEventListener("click", addNumber);
 });
 
 let firstValue = null;
-let getPreviusValue = "";
 let waitingForNumber = false;
+var lastValue = null;
 let operator = "";
 
+function addNumber(event) {
+  if (screen.value === "Error") removeErrorMessage();
 
-function addNumber(event){
-      
-    if(waitingForNumber){
-        screen.value = event.target.textContent;
-        waitingForNumber = false;
-    }
-    else
-        screen.value += event.target.textContent;
+  if (waitingForNumber) {
+    screen.value = event.target.textContent;
+    lastValue += Number(screen.value);
+    waitingForNumber = false;
+  } else screen.value += event.target.textContent;
 
-    operatorButtons.forEach(btn => {
-        btn.classList.remove('active');
-    })
-        
+  operatorButtons.forEach((btn) => {
+    btn.classList.remove("active");
+  });
 }
 
-function doMath(op, clickedButton){
-
-    if(screen.value !== ""){
-         firstValue = parseInt(screen.value);   
-         operator = op;
-         waitingForNumber = true;
-    }
-
-    operatorButtons.forEach(btn => {
-        btn.classList.remove("active");
-    });
-
-    clickedButton.classList.add("active");
+function removeErrorMessage() {
+  screen.value = "";
+  screen.classList.remove("errorMessage");
 }
 
+function doMath(op, clickedButton) {
+  if (screen.value !== "") {
+    firstValue = lastValue != null ? lastValue : Number(screen.value);
+    operator = op;
+    waitingForNumber = true;
+    console.log("Enter the matrix");
+  }
+  changeOperatorFocus(clickedButton);
+}
 
-function calculateResult(){
-    
-    if(firstValue!== null && !waitingForNumber){
-           
-        var lastValue = Number(screen.value);
-        switch(operator){
+function changeOperatorFocus(clickedButton) {
+  operatorButtons.forEach((btn) => {
+    btn.classList.remove("active");
+  });
 
-            case "X":
-                screen.value = firstValue * lastValue;
-                break;
+  clickedButton.classList.add("active");
+}
 
-            case "+":
-                screen.value = firstValue + lastValue;
-                break;
+function calculateResult() {
+  if (firstValue !== null && lastValue !== null) {
+    console.log("calculateResult()");
 
-            case "-":
-                screen.value = firstValue - lastValue;
-                break;
+    switch (operator) {
+      case "X":
+        screen.value = firstValue * lastValue;
+        break;
 
-            case "%":
-                if(lastValue !==0)
-                    screen.value = firstValue / lastValue;
-                else{
-                    screen.classList.add('errorMessage');
-                    screen.value ="Error";
-                }
-                break;
+      case "+":
+        screen.value = firstValue + lastValue;
+        break;
+
+      case "-":
+        screen.value = firstValue - lastValue;
+        break;
+
+      case "%":
+        if (lastValue !== 0) screen.value = firstValue / lastValue;
+        else {
+          screen.classList.add("errorMessage");
+          screen.value = "Error";
         }
-       
-        cleanOperatorUI();
-        firstValue = null;
+        break;
+
+      default:
+        screen.value = "Bug";
     }
-    else
-        throw new Error("You should enter numbers and operator before getting a result.");
-}
 
-function resetValues(){
-    screen.value = "";
     cleanOperatorUI();
-    operator = "";
-    firstValue = null; 
+    firstValue = null;
+  } else
+    throw new Error(
+      "You should enter numbers and operator before getting a result.",
+    );
 }
 
-function cleanOperatorUI(){
-    operatorButtons.forEach(btn => btn.classList.remove("active"));
+function resetValues() {
+  screen.value = "";
+  cleanOperatorUI();
+  operator = "";
+  firstValue = null;
 }
 
-
-
-
+function cleanOperatorUI() {
+  operatorButtons.forEach((btn) => btn.classList.remove("active"));
+}
